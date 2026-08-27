5a6e29208785a8cc53aa3ce2e852819f0a217187
novlan1 <1576271227@qq.com>
2026-08-27 15:25:42 +0800

    chore: add tabbar like vue3-uni

--- src/project/press-ui-pure/router/index.js
+++ src/project/press-ui-pure/router/index.js
@@ -24,6 +24,33 @@ const ROUTER_MAP = [
   },
   getPressPages(),
   getPressPages('act'),
+  {
+    path: '/pages/index/help',
+    component: () => import('@/views/press'),
+    children: [
+      {
+        path: '',
+        component: () => import('src/pages/index/help.vue'),
+        meta: {
+          hideNavigator: true,
+        },
+      },
+    ],
+  },
+  {
+    path: '/pages/index/introduce',
+    component: () => import('@/views/press'),
+    children: [
+      {
+        path: '',
+        component: () => import('src/pages/index/introduce.vue'),
+        meta: {
+          showBack: true,
+          title: 'Press UI',
+        },
+      },
+    ],
+  },
 ];
 
 export default ROUTER_MAP;
--- src/project/press-ui-pure/views/press/index.vue
+++ src/project/press-ui-pure/views/press/index.vue
@@ -5,11 +5,43 @@
     <div class="uni-page-wrapper">
       <router-view />
     </div>
+
+    <div
+      v-if="showTabBar"
+      class="press-tab-bar"
+    >
+      <div
+        v-for="item in tabBarList"
+        :key="item.path"
+        class="press-tab-bar-item"
+        @click="onTabBarClick(item)"
+      >
+        <img
+          class="press-tab-bar-icon"
+          :src="getTabIcon(item)"
+          alt=""
+        >
+        <div
+          class="press-tab-bar-text"
+          :style="{ color: isActiveTab(item) ? TAB_SELECTED_COLOR : TAB_COLOR }"
+        >
+          {{ item.text }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import PressHeader from './header.vue';
+
+const assemblyDefault = require('src/static/tabbar/assembly_default_3x.png');
+const assemblySelected = require('src/static/tabbar/assembly_selected_3x.png');
+const myDefault = require('src/static/tabbar/my_default_3x.png');
+const mySelected = require('src/static/tabbar/my_selected_3x.png');
+
+const TAB_COLOR = '#333333';
+const TAB_SELECTED_COLOR = '#465CFF';
 
 export default {
   name: 'PressWrap',
@@ -17,12 +49,44 @@ export default {
     PressHeader,
   },
   data() {
-    return {};
+    return {
+      TAB_COLOR,
+      TAB_SELECTED_COLOR,
+      tabBarList: [
+        {
+          path: '/pages/press',
+          text: '组件',
+          iconPath: assemblyDefault,
+          selectedIconPath: assemblySelected,
+        },
+        {
+          path: '/pages/index/help',
+          text: '帮助',
+          iconPath: myDefault,
+          selectedIconPath: mySelected,
+        },
+      ],
+    };
+  },
+  computed: {
+    showTabBar() {
+      return this.tabBarList.some(item => item.path === this.$route.path);
+    },
   },
   mounted() {
 
   },
   methods: {
+    isActiveTab(item) {
+      return item.path === this.$route.path;
+    },
+    getTabIcon(item) {
+      return this.isActiveTab(item) ? item.selectedIconPath : item.iconPath;
+    },
+    onTabBarClick(item) {
+      if (item.path === this.$route.path) return;
+      this.$router.push(item.path);
+    },
   },
 };
 </script>
@@ -41,5 +105,38 @@ export default {
   height: calc(100% - 44px - constant(safe-area-inset-top));
   height: calc(100% - 44px - env(safe-area-inset-top));
   overflow: auto;
+}
+
+.press-tab-bar {
+  flex-shrink: 0;
+  display: flex;
+  height: 50px;
+  height: calc(50px + constant(safe-area-inset-bottom));
+  height: calc(50px + env(safe-area-inset-bottom));
+  padding-bottom: constant(safe-area-inset-bottom);
+  padding-bottom: env(safe-area-inset-bottom);
+  background-color: #FFFFFF;
+  border-top: 1px solid #ebedf0;
+  box-sizing: border-box;
+}
+
+.press-tab-bar-item {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.press-tab-bar-icon {
+  width: 28px;
+  height: 28px;
+}
+
+.press-tab-bar-text {
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1;
 }
 </style>
